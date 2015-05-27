3d1987d78f8ed499cdcea7b0f6717689c90003d1
Sophie Chor <sokpheakc@yaho.com>
2015-05-27 02:47:35 -0700

    created b_connect app with a mvc

---
+++ db/migrate/20150527083333_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :password
+      t.string :about_me
+      t.string :blog_page
+      t.string :profile_pic
+
+      t.timestamps null: false
+    end
+  end
+end
