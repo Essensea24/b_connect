0e6858fa1ac0f5839cd1c48649751397f3b465c8
Sophie Chor <sokpheakc@yaho.com>
2015-05-28 23:52:36 -0700

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password #put in a required encrypted password
+	has_many :blogs
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :username, presence: true
+	validates :email, presence: true
+	validates :about_me, presence: true,
+			length: { minimum: 5 }
+			
+
+                    
 end
