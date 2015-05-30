2583a845c09bc1c848db0a5ff023f9f0478e0eab
Essensea24 <sophie.chor24@gmail.com>
2015-05-30 11:04:10 -0700

    added delete tool to user after the login

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -40,11 +40,11 @@ class BlogsController < ApplicationController
 
     # end
 
-    # def destroy
-    # # @user = User.find(params[:user_id])
-    #   @blog.destroy
-    #   redirect_to blogs_path
-    # end
+    def destroy
+      @user =  User.find(session[:user_id])
+      @blog.destroy
+      redirect_to user_path(@user.id)
+    end
 
     private
       # Use callbacks to share common setup or constraints between actions.
