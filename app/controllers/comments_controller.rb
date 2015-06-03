97b3d2a3bb2f6db19bc8a99eac52e2f22f79e900
Essensea24 <sophie.chor24@gmail.com>
2015-06-03 12:30:29 -0700

    added def new and create to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
+ 
   def new
+    @user_comment = get_user.comments.build
+    @blog_comment = get_blog.comments.build
   end
 
   def create
+    @user_comment = get_user.comments.create(comment_params)
+    @blog_comment = get_blog.comments.create(comment_params)
+
+    if @user_comment.save & @blog_comment.save
+      redirect to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +24,22 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private 
+    def comment_params
+      params.require(:comment).permit(:username, :body)
+    end
+
+    def get_user
+      User.find(params[:user_id])
+    end
+
+    def get_blog
+      Blog.find(params[:blog_id])
+    end
+
+    def get_comment
+      Comment.find(params[:id])
+    end
+
 end
