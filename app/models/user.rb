7354c475933936403140f8392a838937ee06e0f0
Essensea24 <sophie.chor24@gmail.com>
2015-06-03 11:04:31 -0700

    adding validation with email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
 	has_secure_password #put in a required encrypted password
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	has_many :blogs
 	has_many :comments
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :username, presence: true
-	validates :email, presence: true
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                uniqueness: { case_sensitive: false }
 	validates :about_me, presence: true,
 			length: { in: 5..200 }
 	validates :password, length: { in: 6..20 }
