60535b2ca078cbdced20b738a6e4f5295a45a63a
Essensea24 <sophie.chor24@gmail.com>
2015-06-23 11:55:48 -0700

    added rspec gem and added a pass test

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+	describe "sessions controller" do 
+		it "should pass the test" do
+			assert true
+		end 
+	end 
+
+end
